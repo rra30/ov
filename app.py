c642e3a0484670c12254cb3fdaa99a40fa281b08
rra30 <163290584+rra30@users.noreply.github.com>
2024-12-21 17:49:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ if source_radio == "IMAGE":
     
     if input is not None:
         uploaded_image = PIL.Image.open(input)
-        uploaded_image_cv =cv2.cvtColor(np.array(uploaded_image), cv2.COLOR_RGB2BGR)
+        uploaded_image_cv =cv2.cvtColor(numpy.array(uploaded_image), cv2.COLOR_RGB2BGR)
         visualized_image = utils.predict_image(uploaded_image_cv, conf_threshold = conf_threshold)
         st.image(visualized_image, channels = "BGR")
 
@@ -64,12 +64,5 @@ if source_radio == "VIDEO":
             pass
 
 
-def play_live_camera():
-    image = camera_input_live()
-    uploaded_image = PIL.Image.open(image)
-    uploaded_image_cv = cv2.cvtColor(np.array(uploaded_image), cv2.COLOR_RGB2BGR)
-    visualized_image = utils.predict_image(uploaded_image_cv, conf_threshold)
-    st.image(visualized_image, channels = "BGR")
-
 if source_radio == "WEBCAM":
-    play_live_camera()
+    play_video(0)
